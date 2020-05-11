f5e91e8b476822cf9902e5d8140ba31949f19a43
Andrew Barker <barkeras@mail.uc.edu>
2020-05-11 16:38:16 -0400

    initial commit

---
+++ js/map.markers.js
@@ -0,0 +1,45 @@
+//List of map markers
+var map_markers = [
+	{
+		"title": "Ceremony and Reception",
+		"latitude":39.1096975,
+		"longitude":-84.5075317,
+		"icon":"fa-bell", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Bell Event Centre <br> 444 Reading Rd, Cincinnati, OH 45202"
+	},
+	{
+		"title": "Symphony Hotel & Restaurant",
+		"latitude":39.1102183,
+		"longitude":-84.51934,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Symphony Hotel & Restaurant <br> 210 W 14th St, Cincinnati, OH 45202"
+	},
+	{
+		"title": "Accommodation 2",
+		"latitude":33.780428,
+		"longitude":-118.084075,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Accommodation 2 <br> Los Alamitos, CA 90720"
+	},
+	{
+		"title": "Accommodation 3",
+		"latitude":33.777551,
+		"longitude":-118.050468,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Accommodation 3 <br> Seal Beach, CA 90740"
+	},
+	{
+		"title": "Transportation",
+		"latitude":33.782930,
+		"longitude":-118.060552,
+		"icon":"fa-plane", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Seal Beach VORTAC SLI <br> Los Alamitos, CA 90720"
+	},
+	{
+		"title": "Ceremony",
+		"latitude":33.776384,
+		"longitude":-118.058149,
+		"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Birchwood Church <br> Seal Beach, CA 90740"
+	}
+];
---
+++ js/variables.js
@@ -0,0 +1,32 @@
+/**
+ * Javascript template variables
+ */
+
+var	mobileMenuTitle = "Menu",					//The title of the mobile menu
+
+	hero100PercentHeight = true,				//If true, the hero section (home) will be set with a minimum height of 100% window height. If false, hero height will be the height of its content.
+
+	//TWITTER VARIABLE
+	twitter_username = "itest",				//Replace with your own Twitter username
+
+
+	//GOOGLE MAP VARIABLES
+	map_canvas_id = "map_canvas",				//The HTML "id" of the map canvas
+	map_color = "#860e1b",						//Google map color
+	map_initial_zoom = 16,						//The initial zoom when Google map loads
+	map_initial_latitude = 39.1096975,			//Google map initial Latitude. If "null", latitude of the first marked will be used
+	map_initial_longitude = -84.5095836,		//Google map initial Longitude. If "null", longitude of the first marked will be used
+	use_default_map_style = false,				//If true, default map style will be used
+
+
+	//CONTACT FORM VARIABLES
+	contact_form_success_msg = "Successfully submitted the form",
+	contact_form_error_msg = "Error submitting form",
+
+
+	//COUNTDOWN VARIABLES
+	c_days = "DAYS",							//Countdown "Days" label
+	c_hours = "HOURS",							//Countdown "Hours" label
+	c_minutes = "MIN.",							//Countdown "Minutes" label
+	c_seconds = "SEC.",							//Countdown "Seconds" label
+	cdownEndMsg = "Wedding Started!";			//Message to display when the countdown reaches the end
