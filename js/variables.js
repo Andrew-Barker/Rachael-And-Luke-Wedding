4fb9e7ff8e635f14d91c1ea95ed4bde5dac8d3f3
Barker, Andrew S <andrew.barker@kroger.com>
2020-05-14 00:57:33 -0400

    updated bridal party order and mobile view

--- js/variables.js
+++ js/variables.js
@@ -2,21 +2,21 @@
  * Javascript template variables
  */
 
-var	mobileMenuTitle = "Menu",					//The title of the mobile menu
+var mobileMenuTitle = "Menu", //The title of the mobile menu
 
-	hero100PercentHeight = true,				//If true, the hero section (home) will be set with a minimum height of 100% window height. If false, hero height will be the height of its content.
+	hero100PercentHeight = true, //If true, the hero section (home) will be set with a minimum height of 100% window height. If false, hero height will be the height of its content.
 
 	//TWITTER VARIABLE
-	twitter_username = "itest",				//Replace with your own Twitter username
+	twitter_username = "itest", //Replace with your own Twitter username
 
 
 	//GOOGLE MAP VARIABLES
-	map_canvas_id = "map_canvas",				//The HTML "id" of the map canvas
-	map_color = "#860e1b",						//Google map color
-	map_initial_zoom = 16,						//The initial zoom when Google map loads
-	map_initial_latitude = 39.1096975,			//Google map initial Latitude. If "null", latitude of the first marked will be used
-	map_initial_longitude = -84.5095836,		//Google map initial Longitude. If "null", longitude of the first marked will be used
-	use_default_map_style = false,				//If true, default map style will be used
+	map_canvas_id = "map_canvas", //The HTML "id" of the map canvas
+	map_color = "#374e7a", //Google map color
+	map_initial_zoom = 16, //The initial zoom when Google map loads
+	map_initial_latitude = 39.0881394, //Google map initial Latitude. If "null", latitude of the first marked will be used
+	map_initial_longitude = -84.4896427, //Google map initial Longitude. If "null", longitude of the first marked will be used
+	use_default_map_style = false, //If true, default map style will be used
 
 
 	//CONTACT FORM VARIABLES
@@ -25,8 +25,8 @@ var	mobileMenuTitle = "Menu",					//The title of the mobile menu
 
 
 	//COUNTDOWN VARIABLES
-	c_days = "DAYS",							//Countdown "Days" label
-	c_hours = "HOURS",							//Countdown "Hours" label
-	c_minutes = "MIN.",							//Countdown "Minutes" label
-	c_seconds = "SEC.",							//Countdown "Seconds" label
-	cdownEndMsg = "Wedding Started!";			//Message to display when the countdown reaches the end
+	c_days = "DAYS", //Countdown "Days" label
+	c_hours = "HOURS", //Countdown "Hours" label
+	c_minutes = "MIN.", //Countdown "Minutes" label
+	c_seconds = "SEC.", //Countdown "Seconds" label
+	cdownEndMsg = "Wedding Started!"; //Message to display when the countdown reaches the end
